e4a5f81ddbbee45c1dc4fabd152c5df32b3b4398
sihuafeng <sihuafeng@HF-Mac-mini.local>
2017-03-09 08:44:50 +0800

    Initial Commit

---
+++ HFCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HFCollectionView
+//
+//  Created by 司华锋 on 2017/3/9.
+//  Copyright © 2017年 HF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HFCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HFCollectionView
+//
+//  Created by 司华锋 on 2017/3/9.
+//  Copyright © 2017年 HF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
